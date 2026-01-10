f325357a4087d06b591ef632f6f0bf56231a3e91
margooey <234476012+margooey@users.noreply.github.com>
2026-01-09 19:38:30 -0500

    feature flags

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 pub mod cg_error;
+#[cfg(feature = "cgs_connection")]
 pub mod cgs_connection;
+#[cfg(feature = "cgs_cursor")]
 pub mod cgs_cursor;
 
 pub use cg_error::*;
+#[cfg(feature = "cgs_connection")]
 pub use cgs_connection::*;
-// pub use cgs_cursor::*;
+#[cfg(feature = "cgs_cursor")]
+pub use cgs_cursor::*;
